2a5227e7033eceabfabcd3643c7ccfc872dcb9cd
Luiz Felipe Machado <luizfelmach@gmail.com>
2026-05-16 13:06:27 -0300

    feat(proxy): add Envoy static config templates for API and Studio proxies

--- internal/controller/proxy_builder.go
+++ internal/controller/proxy_builder.go
@@ -1,9 +1,10 @@
 package controller
 
 import (
+	_ "embed"
 	"fmt"
+	"strings"
 
-	"gopkg.in/yaml.v3"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,15 +19,14 @@ const (
 	proxyPort            = int32(8080)
 	proxyConfigFile      = "envoy.yaml"
 	proxyConfigMountPath = "/etc/envoy"
+	proxyTemplatePath    = "/templates"
 )
 
-type proxyRoute struct {
-	Prefix        string
-	Cluster       string
-	RewritePrefix *string
-}
+//go:embed proxy_templates/api_proxy.yaml
+var apiProxyTemplate string
 
-func strPtr(v string) *string { return &v }
+//go:embed proxy_templates/studio_proxy.yaml
+var studioProxyTemplate string
 
 func proxyLabels(project *platformv1alpha1.Project, proxyType string) map[string]string {
 	return map[string]string{
@@ -41,204 +41,68 @@ func proxyServiceName(projectName, proxyType string) string {
 	return fmt.Sprintf("%s-%s", projectName, proxyType)
 }
 
-func buildEnvoyConfig(routes []proxyRoute, clusters []envoyCluster) (string, error) {
-	envoyRoutes := make([]map[string]any, 0, len(routes))
-	for _, r := range routes {
-		route := map[string]any{
-			"match": map[string]any{"prefix": r.Prefix},
-			"route": map[string]any{"cluster": r.Cluster},
-		}
-		if r.RewritePrefix != nil {
-			route["route"].(map[string]any)["prefix_rewrite"] = *r.RewritePrefix
-		}
-		envoyRoutes = append(envoyRoutes, route)
-	}
-
-	envoyClusters := make([]map[string]any, 0, len(clusters))
-	for _, c := range clusters {
-		cluster := map[string]any{
-			"name":            c.Name,
-			"connect_timeout": "5s",
-			"type":            "STRICT_DNS",
-			"lb_policy":       "ROUND_ROBIN",
-			"load_assignment": map[string]any{
-				"cluster_name": c.Name,
-				"endpoints": []map[string]any{
-					{
-						"lb_endpoints": []map[string]any{
-							{
-								"endpoint": map[string]any{
-									"address": map[string]any{
-										"socket_address": map[string]any{
-											"address":    c.Address,
-											"port_value": c.Port,
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		}
-		envoyClusters = append(envoyClusters, cluster)
-	}
-
-	config := map[string]any{
-		"static_resources": map[string]any{
-			"listeners": []map[string]any{
-				{
-					"address": map[string]any{
-						"socket_address": map[string]any{
-							"address":    "0.0.0.0",
-							"port_value": proxyPort,
-						},
-					},
-					"filter_chains": []map[string]any{
-						{
-							"filters": []map[string]any{
-								{
-									"name": "envoy.filters.network.http_connection_manager",
-									"typed_config": map[string]any{
-										"@type":       "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager",
-										"stat_prefix": "ingress_http",
-										"route_config": map[string]any{
-											"name": "local_route",
-											"virtual_hosts": []map[string]any{
-												{
-													"name":    "backend",
-													"domains": []string{"*"},
-													"routes":  envoyRoutes,
-												},
-											},
-										},
-										"http_filters": []map[string]any{
-											{
-												"name": "envoy.filters.http.router",
-												"typed_config": map[string]any{
-													"@type": "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router",
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-			"clusters": envoyClusters,
-		},
-	}
-
-	bytes, err := yaml.Marshal(config)
-	if err != nil {
-		return "", fmt.Errorf("marshalling envoy config: %w", err)
-	}
-	return string(bytes), nil
+func serviceHost(projectName, namespace, component string) string {
+	return fmt.Sprintf("%s.%s.svc.cluster.local", componentServiceName(projectName, component), namespace)
 }
 
-type envoyCluster struct {
-	Name    string
-	Address string
-	Port    int32
+func proxyConfigMapName(projectName, proxyType string) string {
+	return proxyServiceName(projectName, proxyType)
 }
 
-func buildAPIProxyRoutesAndClusters(project *platformv1alpha1.Project) ([]proxyRoute, []envoyCluster) {
-	ns := project.Namespace
-	name := project.Name
-
-	routes := []proxyRoute{}
-	clusters := []envoyCluster{}
-
-	authEnabled := project.Spec.Auth == nil || derefBool(project.Spec.Auth.Enabled, true)
-	if authEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/auth/v1", Cluster: "auth", RewritePrefix: strPtr("/")})
-		clusters = append(clusters, envoyCluster{Name: "auth", Address: componentServiceName(name, "auth") + "." + ns + ".svc.cluster.local", Port: 9999})
-	}
-
-	restEnabled := project.Spec.Rest == nil || derefBool(project.Spec.Rest.Enabled, true)
-	if restEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/rest/v1", Cluster: "rest", RewritePrefix: strPtr("/")})
-		routes = append(routes, proxyRoute{Prefix: "/graphql/v1", Cluster: "rest", RewritePrefix: strPtr("/rpc/graphql")})
-		clusters = append(clusters, envoyCluster{Name: "rest", Address: componentServiceName(name, "rest") + "." + ns + ".svc.cluster.local", Port: 3000})
-	}
-
-	realtimeEnabled := project.Spec.Realtime == nil || derefBool(project.Spec.Realtime.Enabled, true)
-	if realtimeEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/realtime/v1/api", Cluster: "realtime", RewritePrefix: strPtr("/api")})
-		routes = append(routes, proxyRoute{Prefix: "/realtime/v1", Cluster: "realtime", RewritePrefix: strPtr("/socket")})
-		clusters = append(clusters, envoyCluster{Name: "realtime", Address: componentServiceName(name, "realtime") + "." + ns + ".svc.cluster.local", Port: 4000})
-	}
-
-	storageEnabled := project.Spec.Storage == nil || derefBool(project.Spec.Storage.Enabled, true)
-	if storageEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/storage/v1", Cluster: "storage", RewritePrefix: strPtr("/")})
-		clusters = append(clusters, envoyCluster{Name: "storage", Address: componentServiceName(name, "storage") + "." + ns + ".svc.cluster.local", Port: 5000})
-	}
-
-	functionsEnabled := project.Spec.Functions == nil || derefBool(project.Spec.Functions.Enabled, true)
-	if functionsEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/functions/v1", Cluster: "functions", RewritePrefix: strPtr("/")})
-		clusters = append(clusters, envoyCluster{Name: "functions", Address: componentServiceName(name, "functions") + "." + ns + ".svc.cluster.local", Port: 9000})
-	}
-
-	metaEnabled := project.Spec.Meta == nil || derefBool(project.Spec.Meta.Enabled, true)
-	if metaEnabled {
-		routes = append(routes, proxyRoute{Prefix: "/pg", Cluster: "meta", RewritePrefix: strPtr("/")})
-		clusters = append(clusters, envoyCluster{Name: "meta", Address: componentServiceName(name, "meta") + "." + ns + ".svc.cluster.local", Port: 8080})
-	}
-
-	return routes, clusters
-}
-
-func buildStudioProxyRoutesAndClusters(project *platformv1alpha1.Project) ([]proxyRoute, []envoyCluster) {
-	ns := project.Namespace
-	name := project.Name
-
-	routes := []proxyRoute{
-		{Prefix: "/", Cluster: "studio"},
-	}
-	clusters := []envoyCluster{
-		{Name: "studio", Address: componentServiceName(name, "studio") + "." + ns + ".svc.cluster.local", Port: 3000},
-	}
-
-	return routes, clusters
-}
-
-func buildProxyConfigMap(project *platformv1alpha1.Project, proxyType string) (*corev1.ConfigMap, error) {
-	var routes []proxyRoute
-	var clusters []envoyCluster
-
-	switch proxyType {
-	case proxyAPIComponent:
-		routes, clusters = buildAPIProxyRoutesAndClusters(project)
-	case proxyStudioComponent:
-		routes, clusters = buildStudioProxyRoutesAndClusters(project)
-	default:
-		return nil, fmt.Errorf("unknown proxy type: %s", proxyType)
-	}
-
-	config, err := buildEnvoyConfig(routes, clusters)
-	if err != nil {
-		return nil, err
-	}
-
+func buildProxyConfigMap(project *platformv1alpha1.Project, proxyType string) *corev1.ConfigMap {
 	labels := proxyLabels(project, proxyType)
-	cm := &corev1.ConfigMap{
+	var tmpl string
+	if proxyType == proxyAPIComponent {
+		tmpl = apiProxyTemplate
+	} else {
+		tmpl = studioProxyTemplate
+	}
+	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      proxyServiceName(project.Name, proxyType),
+			Name:      proxyConfigMapName(project.Name, proxyType),
 			Namespace: project.Namespace,
 			Labels:    labels,
 		},
 		Data: map[string]string{
-			proxyConfigFile: config,
+			proxyConfigFile: tmpl,
 		},
 	}
-	return cm, nil
 }
 
-func buildProxyDeployment(project *platformv1alpha1.Project, proxyType string, image string, replicas *int32, resources corev1.ResourceRequirements) *appsv1.Deployment {
+func buildInitScript(proxyType string) string {
+	vars := []string{
+		"AUTH_ADDRESS",
+		"REST_ADDRESS",
+		"REALTIME_ADDRESS",
+		"STORAGE_ADDRESS",
+		"FUNCTIONS_ADDRESS",
+		"META_ADDRESS",
+		"ANON_KEY",
+		"ANON_KEY_ASYMMETRIC",
+		"SERVICE_ROLE_KEY",
+		"SERVICE_ROLE_KEY_ASYMMETRIC",
+		"SUPABASE_PUBLISHABLE_KEY",
+		"SUPABASE_SECRET_KEY",
+	}
+	if proxyType == proxyStudioComponent {
+		vars = append(vars, "STUDIO_ADDRESS", "DASHBOARD_BASIC_AUTH")
+	}
+
+	var sedArgs []string
+	for _, v := range vars {
+		sedArgs = append(sedArgs, fmt.Sprintf(`-e "s|\${%s}|${%s}|g"`, v, v))
+	}
+
+	return fmt.Sprintf(
+		"cp %s/%s %s/%s && sed -i %s %s/%s",
+		proxyTemplatePath, proxyConfigFile,
+		proxyConfigMountPath, proxyConfigFile,
+		strings.Join(sedArgs, " "),
+		proxyConfigMountPath, proxyConfigFile,
+	)
+}
+
+func buildProxyDeployment(project *platformv1alpha1.Project, proxyType, image string, replicas *int32, resources corev1.ResourceRequirements, env []corev1.EnvVar) *appsv1.Deployment {
 	labels := proxyLabels(project, proxyType)
 	r := derefInt32(replicas, 1)
 	return &appsv1.Deployment{
@@ -253,6 +117,16 @@ func buildProxyDeployment(project *platformv1alpha1.Project, proxyType string, i
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{Labels: labels},
 				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{{
+						Name:    "envoy-init",
+						Image:   image,
+						Command: []string{"sh", "-c", buildInitScript(proxyType)},
+						Env:     env,
+						VolumeMounts: []corev1.VolumeMount{
+							{Name: "config-template", MountPath: proxyTemplatePath},
+							{Name: "config", MountPath: proxyConfigMountPath},
+						},
+					}},
 					Containers: []corev1.Container{{
 						Name:  proxyType,
 						Image: image,
@@ -268,16 +142,24 @@ func buildProxyDeployment(project *platformv1alpha1.Project, proxyType string, i
 						}},
 						Args: []string{"-c", proxyConfigMountPath + "/" + proxyConfigFile},
 					}},
-					Volumes: []corev1.Volume{{
-						Name: "config",
-						VolumeSource: corev1.VolumeSource{
-							ConfigMap: &corev1.ConfigMapVolumeSource{
-								LocalObjectReference: corev1.LocalObjectReference{
-									Name: proxyServiceName(project.Name, proxyType),
+					Volumes: []corev1.Volume{
+						{
+							Name: "config-template",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: proxyConfigMapName(project.Name, proxyType),
+									},
 								},
 							},
 						},
-					}},
+						{
+							Name: "config",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+					},
 				},
 			},
 		},
